8bc123737343c2422e63ffe32b83c9455bf58121
aamubarak81-coder <aa.mubarak81@gmail.com>
2026-07-03 20:49:47 +0300

    Refactor main.dart for improved readability and updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
-﻿import 'screens/main_navigation_screen.dart';
-import 'screens/main_navigation_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'screens/main_navigation_screen.dart';
 import 'screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // طھظ… ط¯ظ…ط¬ ط§ظ„ط±ط§ط¨ط· ظ‡ظ†ط§ ظƒظƒظ„ظ…ط© ظˆط§ط­ط¯ط© ظ…طھطµظ„ط© طھظ…ط§ظ…ط§ظ‹ ط¨ط¯ظˆظ† ط£ظٹ ظ…ط³ط§ظپط§طھ
+  // تهيئة الاتصال بقاعدة بيانات Supabase
   await Supabase.initialize(
     url: 'https://dqyorsbkxxgjctprjjdw.supabase.co',
-    publishableKey: 'sb_publishable_8RMW-u8_2wdFG_pa4RVjYA_mJ5yx1OF',   
+    publishableKey: 'sb_publishable_8RMW-u8_2wdFG_pa4RVjYA_mJ5yx1OF',
   );
 
   runApp(const MyApp());
@@ -24,13 +23,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ظ†ط¸ط§ظ… ط§ظ„ط¬ط±ط¯ ط§ظ„ط°ظƒظٹ',
+      title: 'نظام الجرد الذكي',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
